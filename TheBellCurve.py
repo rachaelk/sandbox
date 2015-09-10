aac35f99d8056c24727986ad115235e8c88917ba
Rachael Keller <rkell18@lsu.edu>
2015-09-10 09:07:18 -0400

    Added new data set from NLS survey data and code for the renaming of the columns in that data set.

--- TheBellCurve.py
+++ TheBellCurve.py
@@ -6,7 +6,7 @@ import numpy as np
 #	  AFQT is used by armed forces in MOS selection; so-called "g-loaded"; numeric value represents
 #     the testers position relative to the population of test-takers (not limited to the sample)
 
-NLYSData = pd.read_csv('C:\Users\Charmander\Downloads\NLSY79DataSet\NLSY79DataSet.csv', na_values = [-4])
+NLYSData = pd.read_csv('NLSY79DataSets/NLSY79Data0.csv', na_values = [-4])
 
 NLYSData.columns = ["Subject", "Ethnicity", "Gender", "AFQT \'79", "AFQT '90", "AFQT '99"]
 
@@ -15,6 +15,8 @@ NLYSData = NLYSData.set_index("Subject")
 BCData = NLYSData[["Ethnicity", "AFQT '90"]].dropna()
 
 BCData.to_csv("BellCurveData.csv")
+# Added to check that the same results would be produced using the data set I pulled.
+# BCData.to_csv("BellCurveData_trial.csv")
 
 
 
